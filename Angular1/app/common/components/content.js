7c1b70bef0606cbb8dd20d3cf90a0a9e4817c834
RAFAEL CAMARA <rafaelcalado25@gmail.com>
2017-11-03 14:21:38 -0200

    Frant end finalizado sem a integração com o backend

---
+++ Angular1/app/common/components/content.js
@@ -0,0 +1,32 @@
+angular.module('MinhaApp').component('content', {
+  bindings: {
+    grid: '@',
+    color: '@',
+    valor: '@',
+    tipoConta: '@',
+    icone: '@',
+  },
+  controller: [
+    'myFactory',
+    function (myFactory) {
+      //Este controller não funciona, pois o valor da grid
+      // ainda não foi passado pelo component
+      //Para funcionar temos que fazer o código abaixo que sicroniza
+      //a passagem de parametro com a inicialização do component
+      this.$onInit = () => this.gridClasses = myFactory.toCssClasses(this.grid);      
+    },
+  ],
+  template: `
+  <div class="{{ $ctrl.gridClasses }}">
+  <div class="small-box {{ $ctrl.color }}">
+    <div class="inner">
+      <h3>{{ $ctrl.valor }}</h3>
+      <p>{{ $ctrl.tipoConta }}</p>
+    </div>
+    <div class="icon">
+      <i class="{{ $ctrl.icone }}"></i>
+    </div>
+  </div>           
+</div> 
+`,
+});
